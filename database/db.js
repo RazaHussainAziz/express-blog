ed86819d23bd6c2b95e3960e4876461ba01e4ce1
unknown <razageeksforgeeks@gmail.com>
2024-04-07 14:23:42 +0500

    first commit

--- database/db.js
+++ database/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+//function for database connection
 const dbConnection = function(URI){
     mongoose.connect(URI).then(()=>console.log("⚙ DB Connected")).catch(()=>"DB Connection Failed");
 }
